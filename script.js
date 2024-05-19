e56776283c2e88d15dbbd8df313ebb6202d3742d
aybshb <126345750+aybshb@users.noreply.github.com>
2024-05-19 22:57:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCsMDC9RA-NlpZB3l2JNOcPzCmXGGmhu0k",
-    authDomain: "safekey-2285f.firebaseapp.com",
-    projectId: "safekey-2285f",
-    storageBucket: "safekey-2285f.appspot.com",
-    messagingSenderId: "518367541621",
-    appId: "1:518367541621:web:0c4d3495035495c078fb3c",
-    measurementId: "G-9K5FVJQCTW"
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore(app);
-
 document.getElementById('notifyForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const email = document.getElementById('email').value;
