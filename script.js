371abd3a15a69d730c866fbff7941d8c145bb626
aybshb <126345750+aybshb@users.noreply.github.com>
2024-05-19 23:29:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/script";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore} from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
