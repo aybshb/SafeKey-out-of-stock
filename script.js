2f62a1754c7cad8121a2fddd7a742f43383589ed
aybshb <126345750+aybshb@users.noreply.github.com>
2024-05-19 22:04:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-function handleSubmit(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-    var form = event.target;
-    var submitted = false;
-
-    // Create a new FormData object to hold the form data
-    var formData = new FormData(form);
-
-    // Send the form data using fetch
-    fetch(form.action, {
-        method: form.method,
-        body: formData,
-        mode: 'no-cors' // Important: Use 'no-cors' to prevent CORS issues
-    }).then(function() {
-        // Display the success message
-        document.getElementById('message').textContent = 'Thank you! You will be notified when SafeKey is back in stock.';
-        form.reset(); // Clear the input field
-    }).catch(function(error) {
-        console.error('Error:', error);
-    });
-
-    return false; // Prevent the default form submission
+function handleFormSubmit() {
+    // Display a success message immediately upon form submission
+    document.getElementById('message').textContent = 'Thank you! You will be notified when SafeKey is back in stock.';
+    document.getElementById('notifyForm').reset(); // Clear the input field
 }
