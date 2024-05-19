5778e195fdaf3d59ca560c6662887fe61d5d7fdc
aybshb <126345750+aybshb@users.noreply.github.com>
2024-05-19 22:25:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ var submitted = false;
 
 // Function to handle form submission
 function handleFormSubmit(event) {
+    Console.log('kaisa hai1')
     event.preventDefault(); // Prevent the default form submission behavior
     submitted = true; // Set the submitted flag to true
     document.getElementById('notifyForm').submit(); // Submit the form
+    Console.log('kaisa hai2')
 }
 
 // Function to display the success message after the form is submitted
 function displaySuccessMessage() {
     if (submitted) {
+        Console.log('kaisahai3')
         document.getElementById('message').textContent = 'Thank you! You will be notified when SafeKey is back in stock.';
         document.getElementById('notifyForm').reset(); // Clear the input field
         submitted = false; // Reset the submitted flag
+        Console.log('kaisa hai4')
     }
 }
 
