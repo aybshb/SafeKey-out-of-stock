fe81df83a6f05623ee2ebbc5e2e53543f987ea80
aybshb <126345750+aybshb@users.noreply.github.com>
2024-05-19 22:32:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-document.getElementById('notifyForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.querySelector('input[name="email"]').value;
-    const formData = new FormData();
-    formData.append('entry.1308858926', email);
+var submitted = false;
 
-    fetch('https://docs.google.com/forms/d/e/1FAIpQLSfiH7H7I5SivNjoakOYrw6uWtTZcythHqFJAXzTjv_0a6H5Zg/formResponse', {
-        method: 'POST',
-        mode: 'no-cors',
-        body: formData
-    })
-    .then(response => {
-        // As no-cors mode won't give us response data, we assume success
-        document.getElementById('message').textContent = 'Thank you! You will be notified when SafeKey is back in stock.';
-        document.getElementById('notifyForm').reset(); // Clear the input field
-    })
-    .catch(error => {
-        document.getElementById('message').textContent = 'An error occurred. Please try again.';
-    });
+document.getElementById('notifyForm').addEventListener('submit', function(event) {
+    submitted = true;
 });
+
+function showThankYouMessage() {
+    document.getElementById('message').textContent = 'Thank you! You will be notified when SafeKey is back in stock.';
+    document.getElementById('notifyForm').reset(); // Clear the input field
+}
