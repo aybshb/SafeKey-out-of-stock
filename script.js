0c458c6a166eae90f70e2f5dc01199bd33178d3c
aybshb <126345750+aybshb@users.noreply.github.com>
2024-05-19 22:11:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function handleFormSubmit() {
-    console.log('form submitted')
-    // Display a success message immediately upon form submission
-    document.getElementById('message').textContent = 'Thank you! You will be notified when SafeKey is back in stock.';
-    document.getElementById('notifyForm').reset(); // Clear the input field
+// Variable to track form submission
+var submitted = false;
+
+// Function to handle form submission
+function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    submitted = true; // Set the submitted flag to true
+    document.getElementById('notifyForm').submit(); // Submit the form
 }
+
+// Function to display the success message after the form is submitted
+function displaySuccessMessage() {
+    if (submitted) {
+        document.getElementById('message').textContent = 'Thank you! You will be notified when SafeKey is back in stock.';
+        document.getElementById('notifyForm').reset(); // Clear the input field
+        submitted = false; // Reset the submitted flag
+    }
+}
+
+// Add an event listener to the iframe to call displaySuccessMessage on load
+document.getElementById('hidden_iframe').addEventListener('load', displaySuccessMessage);
