685813ed9f661b67946bf8b859453fcfd399121d
aybshb <126345750+aybshb@users.noreply.github.com>
2024-05-19 21:50:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    document.getElementById('message').textContent = 'Thank you! You will be notified when SafeKey is in stock.';
-    document.querySelector('form').reset(); // Clear the input field
+// Variable to track form submission
+var submitted = false;
+
+// Add an event listener to the form to set the submitted flag to true on submit
+document.getElementById('notifyForm').addEventListener('submit', function(event) {
+    submitted = true;
 });
+
+// Function to display the success message after the form is submitted
+function displaySuccessMessage() {
+    if (submitted) {
+        document.getElementById('message').textContent = 'Thank you! You will be notified when SafeKey is back in stock.';
+        document.getElementById('notifyForm').reset(); // Clear the input field
+        submitted = false; // Reset the submitted flag
+    }
+}
+
+// Add an event listener to the iframe to call displaySuccessMessage on load
+document.getElementById('hidden_iframe').addEventListener('load', displaySuccessMessage);
